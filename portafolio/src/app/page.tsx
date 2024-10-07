5a0f89f6cfdbef9da4fdb2ad2ea6edc99f50b481
Andres Rodriguez <banrostin@gmail.com>
2024-10-07 12:37:49 -0600

    BG de full body y detalles nav bar

--- portafolio/src/app/page.tsx
+++ portafolio/src/app/page.tsx
@@ -8,9 +8,9 @@ export default function Home() {
     <div>
       <section>
         {/* <Image src={} height={} width={} alt=""></Image> */}
-        <div>
-          <h1 className="text-center text-2xl underline">About Me</h1>
-          <p className="text-white text-left p-3 leading-loose">Soy un desarrollador Full Stack Web apasionado por crear soluciones tecnológicas innovadoras y funcionales. Mi propuesta de valor radica en mi capacidad para abordar proyectos desde una perspectiva integral, combinando el desarrollo frontend y backend para construir aplicaciones escalables y de alto rendimiento. Recientemente, finalicé un bootcamp intensivo donde consolidé mis habilidades en tecnologías modernas como React, Next.js, Express, TypeScript, y MongoDB.
+        <div className="flex flex-row space-x-44">
+          <h1 className="text-2xl m-10 ">About Me</h1>
+          <p className="text-white text-left p-3 leading-loose text-lg">Soy un desarrollador Full Stack Web apasionado por crear soluciones tecnológicas innovadoras y funcionales. Mi propuesta de valor radica en mi capacidad para abordar proyectos desde una perspectiva integral, combinando el desarrollo frontend y backend para construir aplicaciones escalables y de alto rendimiento. Recientemente, finalicé un bootcamp intensivo donde consolidé mis habilidades en tecnologías modernas como React, Next.js, Express, TypeScript, y MongoDB.
 
             A lo largo de un año de experiencia, he trabajado tanto en proyectos individuales como colaborativos. Mi enfoque en la resolución de problemas me ha permitido participar en el desarrollo de diversas aplicaciones, desde un gestor de turnos y una página de películas consumiendo APIs, hasta un Ecommerce y una plataforma personalizada para la gestión de eventos de un chef. Estos proyectos han sido fundamentales para perfeccionar mis competencias en integración de APIs, diseño de bases de datos, y despliegue en la nube.
 
--- portafolio/src/components/navbar/Navbar.tsx
+++ portafolio/src/components/navbar/Navbar.tsx
@@ -1,6 +1,6 @@
 //Vendors
 import Link from "next/link"
-
+import Image from "next/image"
 
 
 const Navbar: React.FC = () => {
@@ -8,12 +8,19 @@ const Navbar: React.FC = () => {
     return (
         <div className="">
             <title>Home</title>
-            <ul className="flex flex-col justify-center items-center gap-2 bg-white bg-opacity-20 backdrop-blur-lg m-4 p-4 rounded-md h-auto text-lg text-white
+            <ul className="flex flex-col justify-between items-center gap-2 bg-white bg-opacity-20 backdrop-blur-lg m-10 p-4 rounded-md md:max-h-20 text-lg text-white
             sm:flex-row sm:gap-24 font-serif lg:gap-28
             ">
-                <Link href="/" className="hover:text-neon-blue"><li>Home</li></Link>
-                <Link href="/projects" className="hover:text-neon-blue"><li>Projects</li></Link>
-                <Link href="" className="hover:text-neon-blue"><li>Experience</li></Link>
+                <div className="flex-shrink-0">
+                    <Image src={"/assets/genetic.png"} alt="Gentic logo" height={120} width={120}
+                        className="hover:cursor-pointer"
+                    />
+                </div>
+                <div className="flex md:gap-16 md:mr-8">
+                    <Link href="/" className="hover:text-neon-blue"><li>Home</li></Link>
+                    <Link href="/projects" className="hover:text-neon-blue"><li>Projects</li></Link>
+                    <Link href="" className="hover:text-neon-blue"><li>Experience</li></Link>
+                </div>
             </ul>
 
         </div>
