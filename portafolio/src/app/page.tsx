ce66ec872cf21099f812f8928a356c3fa28a690b
Andres Rodriguez <banrostin@gmail.com>
2024-10-10 11:19:00 -0600

    Hover navbar links, home info ajuste

--- portafolio/src/app/page.tsx
+++ portafolio/src/app/page.tsx
@@ -7,17 +7,18 @@ export default function Home() {
   return (
     <div>
       <section>
-        {/* <Image src={} height={} width={} alt=""></Image> */}
-        <div className="flex flex-row space-x-44">
-          <h1 className="text-2xl m-10 ">About Me</h1>
-          <p className="text-white text-left p-3 leading-loose text-lg">Soy un desarrollador Full Stack Web apasionado por crear soluciones tecnológicas innovadoras y funcionales. Mi propuesta de valor radica en mi capacidad para abordar proyectos desde una perspectiva integral, combinando el desarrollo frontend y backend para construir aplicaciones escalables y de alto rendimiento. Recientemente, finalicé un bootcamp intensivo donde consolidé mis habilidades en tecnologías modernas como React, Next.js, Express, TypeScript, y MongoDB.
-
+        <div className="flex flex-col items-center bg-white bg-opacity-5 backdrop-blur-lg w-[90%] m-10 p-4 rounded-xl">
+          <h1 className="text-2xl whitespace-nowrap text-white p-5 text-center">About Me</h1>
+          <p className="text-white text-left p-3 leading-loose text-lg max-w-4xl">
+            Soy un desarrollador Full Stack Web apasionado por crear soluciones tecnológicas innovadoras y funcionales. Mi propuesta de valor radica en mi capacidad para abordar proyectos desde una perspectiva integral, combinando el desarrollo frontend y backend para construir aplicaciones escalables y de alto rendimiento. Recientemente, finalicé un bootcamp intensivo donde consolidé mis habilidades en tecnologías modernas como React, Next.js, Express, TypeScript, y MongoDB.
+            <br /><br />
             A lo largo de un año de experiencia, he trabajado tanto en proyectos individuales como colaborativos. Mi enfoque en la resolución de problemas me ha permitido participar en el desarrollo de diversas aplicaciones, desde un gestor de turnos y una página de películas consumiendo APIs, hasta un Ecommerce y una plataforma personalizada para la gestión de eventos de un chef. Estos proyectos han sido fundamentales para perfeccionar mis competencias en integración de APIs, diseño de bases de datos, y despliegue en la nube.
-
-            Estoy especialmente interesado en seguir aprendiendo y aplicar mis conocimientos en proyectos desafiantes que me permitan crecer como profesional. Disfruto colaborar en equipos dinámicos donde la creatividad y la eficiencia son claves para el éxito.</p>
+            <br /><br />
+            Estoy especialmente interesado en seguir aprendiendo y aplicar mis conocimientos en proyectos desafiantes que me permitan crecer como profesional. Disfruto colaborar en equipos dinámicos donde la creatividad y la eficiencia son claves para el éxito.
+          </p>
         </div>
       </section>
     </div>
-
   );
 }
+
--- portafolio/src/components/navbar/Navbar.tsx
+++ portafolio/src/components/navbar/Navbar.tsx
@@ -1,30 +1,67 @@
+"use client"
 //Vendors
 import Link from "next/link"
 import Image from "next/image"
+import { useState } from "react"
+import { useEffect } from "react"
 
 
 const Navbar: React.FC = () => {
+    // Estado para controlar la apertura del sidebar
+    const [isOpen, setIsOpen] = useState(false);
+
+    // Función para alternar el sidebar
+    const toggleSidebar = () => {
+        setIsOpen(!isOpen);
+    };
+    
+      
 
     return (
-        <div className="">
-            <title>Home</title>
-            <ul className="flex flex-col justify-between items-center gap-2 bg-white bg-opacity-20 backdrop-blur-lg m-10 p-4 rounded-md md:max-h-20 text-lg text-white
-            sm:flex-row sm:gap-24 font-serif lg:gap-28
+        <div>
+            {/* Barra de navegación */}
+            <ul className="flex flex-col justify-between items-center gap-2 bg-white bg-opacity-5 backdrop-blur-lg m-10 p-4 rounded-md md:max-h-20 text-lg text-white
+            sm:flex-row sm:gap-24 font-serif lg:gap-28 w-[90%]
             ">
-                <div className="flex-shrink-0">
-                    <Image src={"/assets/genetic.png"} alt="Gentic logo" height={120} width={120}
-                        className="hover:cursor-pointer"
-                    />
+                <div className="flex-shrink-0 hover:bg-white hover:bg-opacity-35 rounded-full transition-colors duration-500">
+                    <Image src={"/assets/genetic.png"} alt="Gentic logo" height={120} width={120} className="hover:cursor-pointer
+                    transform transition-transform duration-500 hover:scale-75
+                    "
+                     />
                 </div>
-                <div className="flex md:gap-16 md:mr-8">
-                    <Link href="/" className="hover:text-neon-blue"><li>Home</li></Link>
-                    <Link href="/projects" className="hover:text-neon-blue"><li>Projects</li></Link>
-                    <Link href="" className="hover:text-neon-blue"><li>Experience</li></Link>
+
+                {/* Links del Navbar para pantallas grandes */}
+                <div className="hidden md:flex md:gap-16 md:mr-8 text-xl">
+                    <Link href="/" className="navlink-gradient hover:navlink-gradient-hover"><li>Home</li></Link>
+                    <Link href="/projects" className="navlink-gradient hover:navlink-gradient-hover"><li>Projects</li></Link>
+                    <Link href="" className="navlink-gradient hover:navlink-gradient-hover"><li>Experience</li></Link>
+                </div>
+
+                {/* Ícono de menú hamburguesa para pantallas pequeñas */}
+                <div className="md:hidden">
+                    <button onClick={toggleSidebar}>
+                        <svg className="w-8 h-8 text-white hover:text-cyan-900" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7"></path>
+                        </svg>
+                    </button>
                 </div>
             </ul>
 
+            {/* Sidebar para pantallas pequeñas */}
+            <div className={`fixed top-0 right-0 w-64 h-full bg-white bg-opacity-10 backdrop-blur-lg shadow-lg transform ${isOpen ? 'translate-x-0' : 'translate-x-full'} transition-transform duration-300 ease-in-out z-50`}>
+                <button className="absolute top-4 right-4 text-white" onClick={toggleSidebar}>
+                    <svg className="w-8 h-8" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12"></path>
+                    </svg>
+                </button>
+                <div className="flex flex-col gap-8 mt-20 ml-6 text-white text-lg">
+                    <Link href="/" className="hover:text-neon-blue"><li onClick={toggleSidebar}>Home</li></Link>
+                    <Link href="/projects" className="hover:text-neon-blue"><li onClick={toggleSidebar}>Projects</li></Link>
+                    <Link href="" className="hover:text-neon-blue"><li onClick={toggleSidebar}>Experience</li></Link>
+                </div>
+            </div>
         </div>
     )
 }
 
-export default Navbar
+export default Navbar;
