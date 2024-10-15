24cbee704bd2f55784a2cbedc8e3920b4c3d00f6
Andres Rodriguez <banrostin@gmail.com>
2024-10-14 19:22:56 -0600

    contact, certification section

--- portafolio/src/app/page.tsx
+++ portafolio/src/app/page.tsx
@@ -14,27 +14,35 @@ export default function Home() {
             <br /><br />
             Estoy especialmente interesado en seguir aprendiendo y aplicar mis conocimientos en proyectos desafiantes que me permitan crecer como profesional. Disfruto colaborar en equipos dinámicos donde la creatividad y la eficiencia son claves para el éxito.
           </p>
-          <h1 className="text-3xl whitespace-nowrap text-white p-5 text-center">Curriculum Vitae</h1>
-          <div className="flex flex-row mb-4 gap-8 text-xl text-white">
+          <div className="flex flex-col mb-11 gap-20 text-xl text-white md:flex-row ">
 
-            <div className="flex flex-col items-center space-y-4">
-              <h1>Español</h1>
-              <a href="/assets/Cv-Andres-Rodriguez-Jimenez-E.pdf"
-              target="_blanked"
-              ><Image src={"/assets/pdf-icon.png"} alt="" height={90} width={90}
-                className="transform transition-transform duration-300 hover:translate-y-[-10px]"
-                /></a>
+            <div className="flex flex-col">
+              <h1 className="text-3xl whitespace-nowrap text-white p-5 text-center">Curriculum Vitae</h1>
+
+              <div className="flex flex-row justify-center gap-8">
+                <div className="flex flex-col items-center space-y-4">
+                  <h1>Español</h1>
+                  <a href="/assets/Cv-Andres-Rodriguez-Jimenez-E.pdf"
+                    target="_blanked"
+                  ><Image src={"/assets/pdf-icon.png"} alt="" height={90} width={90}
+                    className="transform transition-transform duration-300 hover:translate-y-[-10px]"
+                    /></a>
+                </div>
+
+                <div className="flex flex-col items-center space-y-4 ">
+                  <h1>Ingles</h1>
+                  <a href="/assets/Cv-Andres-Rodriguez-Jimenez-I.pdf"
+                    target="_blanked"
+                  ><Image src={"/assets/pdf-icon.png"} alt="" height={90} width={90}
+                    className="transform transition-transform duration-300 hover:translate-y-[-10px]"
+                    /></a>
+                </div>
+              </div>
             </div>
 
-            <div className="flex flex-col items-center space-y-4 ">
-              <h1>Ingles</h1>
-              <a href="/assets/Cv-Andres-Rodriguez-Jimenez-I.pdf"
-                  target="_blanked"
-              ><Image src={"/assets/pdf-icon.png"} alt="" height={90} width={90}
-                className="transform transition-transform duration-300 hover:translate-y-[-10px]"
-                /></a>
-            </div>
+
           </div>
+
         </div>
 
         <div>
@@ -101,7 +109,7 @@ export default function Home() {
             transform transition-transform duration-300 hover:translate-y-[-10px] max-w-[100px] sm:max-w-[120px]
             "/></li>
             </div>
-            <div className="w-full border-b-2 border-x-cyan-50 opacity-75"></div>
+            {/* <div className="w-full border-b-2 border-x-cyan-50 opacity-75"></div> */}
           </ul>
 
           <div className="flex flex-col md:flex-row justify-around w-full">
@@ -140,7 +148,61 @@ export default function Home() {
               </ul>
             </div>
           </div>
+          <div className="w-full border-b-2 border-x-cyan-50 opacity-75"></div>
 
+          <h1 className="text-3xl whitespace-nowrap text-white p-5 text-center mb-2">Certifications</h1>
+
+          <div className="flex flex-col gap-10 md:flex-row text-white text-xl mb-8">
+
+            <div className="flex flex-col items-center space-y-6">
+              <h1>Henry Full Stack Developer</h1>
+              <Image src="/assets/certificado-henry.png" alt="henry" height={400} width={350}
+                className="hover:scale-110 transform transition-transform duration-300"
+              />
+            </div>
+
+            <div className="flex flex-col items-center space-y-6">
+              <h1>EF SET Certification</h1>
+              <Image src="/assets/certificado-ingles.png" alt="henry" height={400} width={350}
+                className="hover:scale-110 transform transition-transform duration-300"
+
+              />
+            </div>
+
+            <div className="flex flex-col items-center space-y-6">
+              <h1>GAC Certification</h1>
+              <Image src="/assets/certificado-gac.jpeg" alt="henry" height={400} width={350}
+                className="hover:scale-110 transform transition-transform duration-300"
+
+              />
+            </div>
+
+          </div>
+          <div className="w-full border-b-2 border-x-cyan-50 opacity-75"></div>
+
+          <div className="flex flex-col text-center text-white space-y-4 p-3 ">
+            <h2 className="text-3xl mt-2">Contact me</h2>
+
+            <div className="flex flex-row gap-2 justify-center items-center">
+              <Image src="/assets/github-icon.png" alt="github" height={40} width={40} />
+              <a href="https://github.com/banrostin1312"
+                className="hover:underline hover:scale-105 transform transition-transform duration-300"
+                target="_blanked"
+
+              >GitHub</a>
+            </div>
+            <div className="flex flex-row gap-2 justify-center items-center">
+              <Image src="/assets/linkedin-icon.png" alt="github" height={40} width={40} />
+              <a href="https://www.linkedin.com/in/andres-rodriguez-jimenez-51b41a325/"
+                className="hover:underline hover:scale-105 transform transition-transform duration-300"
+                target="_blanked"
+              >LinkedIn</a>
+            </div>
+            <div className="flex flex-row gap-2 justify-center items-center">
+              <Image src="/assets/email-icon.png" alt="github" height={40} width={40} />
+              <p className="break-all">arodriguezjimenez60@gmail.com</p>
+            </div>
+          </div>
         </div>
       </section>
     </div>
